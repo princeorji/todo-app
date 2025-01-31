5b5c2825fe13cc5e400f504c32d21123011d212c
princeorji <michaelorji8039@gmail.com>
2025-01-31 01:54:16 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const connectDB = require('./config/mongodb');
+const errorHandler = require('./middleware/errorHandler');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.DATABASE_URL)
+        app.listen(port, () => {
+            console.log(`Server listening on port: ${port}`);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err.stack);
+
+    const status = res.statusCode ? res.statusCode : 500;
+
+    res.status(status).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err?.stack
+    })
+}
+
+module.exports = errorHandler;
